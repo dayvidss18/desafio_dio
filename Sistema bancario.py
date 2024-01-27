d577c97ca2c3e9fc55056760b20fa5867a721ce1
dayvidss18 <dayviddss18@gmail.com>
2024-01-27 16:13:31 -0300

    mudanças

--- Sistema bancario.py
+++ Sistema bancario.py
@@ -1,43 +1,79 @@
+from datetime import datetime
 print('''
     ========== BEM VINDO AO SISTEMA BANCARIO DIO ==========
-    =======================================================
-''')
-max_saque = 3
+    =======================================================''')
+max_saque = 0
 limite_saldo_saque = float(500)
 extrato = []
 
 saldo = 1000
 
-print('\n Opções')
-print('1. Sacar')
-print('2. Depositar')
-print('3. Saldo')
-print('4. Extrato')
+menu = '''
+Opções
+[1] Sacar
+[2] Depositar
+[3] Saldo  
+[4] Extrato    
+            
+=> '''
 
-opcao = int(input('O que quer fazer: '))
+
 while True:
+    opcao = int(input(menu))
     if opcao == 1:
-        print('''Ferramenta De Saque Dio''')
-        saque_usuario = int(input('Quanto deseja Sacar: '))
-        saldo = saldo - saque_usuario
-        print('Você realizou o saque de {saque_usuario} reais'.format(saque_usuario = saque_usuario))
-        print('Seu limite agora é de {saldo} reais'.format(saldo = saldo))
-        extrato.append('''Você realizou o saque de {saque_usuario} reais,
-                    Saldo total: {saldo} Reais'''.format(saque_usuario = saque_usuario, saldo = saldo))
+        if max_saque < 3 and limite_saldo_saque < 500:
+            print('''Ferramenta De Saque Dio''')
+            saque_usuario = int(input('Quanto deseja Sacar: '))
+            saldo = saldo - saque_usuario
+            print('''
+              ======================================================
+                     Você realizou o saque de {saque_usuario} reais
+              ======================================================
+              '''.format(saque_usuario = saque_usuario))
+            print('''
+              ======================================================
+                     Seu limite agora é de {saldo} reais
+              ======================================================
+              '''.format(saldo = saldo))
+            print('Você já realizou o maximo de saques hoje')
+            extrato.append('Você realizou o saque de {saque_usuario} reais'.format(saque_usuario = saque_usuario, saldo = saldo))
+            extrato.append('Saldo total: {saldo} Reais'.format(saldo = saldo))
+                    
+            max_saque+=1
+        else:
+            print('Você já realizou o maximo de saques por hoje!')
+      
+
+        
     elif opcao == 2:
         print('''FERRAMENTA DE DEPOSITO DIO''')
         deposito_usuario = int(input('Quanto Deseja Depositar: '))
         saldo = saldo + deposito_usuario
-        print('Você realizou o deposito de {deposito_usuario} reais'.format(deposito_usuario = deposito_usuario))
-        print('Seu saldo atual é de {saldo} reais'.format(saldo = saldo))
+        print('''
+              ========================================================
+                Você realizou o deposito de {deposito_usuario} reais
+              ========================================================
+              '''.format(deposito_usuario = deposito_usuario))
+        print('''
+              ========================================================
+                        Seu saldo atual é de {saldo} reais
+              ========================================================
+              '''.format(saldo = saldo))
+        extrato.append('Você realizou o deposito de  {deposito_usuario} reais'.format(deposito_usuario = deposito_usuario))
+        extrato.append('Saldo total: {saldo}'.format(saldo = saldo))
+
     elif opcao == 3:
         print('''FERRAMENTA DE SALDO DIO''')
-        print('Seu saldo atual é de {saldo} reais'.format(saldo = saldo))
+        print('''
+              ========================================================
+                        Seu saldo atual é de {saldo} reais
+              ========================================================
+              '''.format(saldo = saldo))
     elif opcao == 4:
         print('''FERRAMENTA DE EXTRATO DIO''')
         print(extrato)
     else:
-        print('0')
+        print('Opção Inválida! Tente Novamente')
 
 
 
